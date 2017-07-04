f47a06f63099a53c19f1a8d465a16c5a2750934a
Zac Garby <zacgarby@icloud.com>
2017-07-04 11:52:06 +0100

    Initial Commit

---
+++ iTunes Graphs/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iTunes Graphs
+//
+//  Created by Zac G on 04/07/2017.
+//  Copyright © 2017 Zac G. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ iTunes Graphs/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iTunes Graphs
+//
+//  Created by Zac G on 04/07/2017.
+//  Copyright © 2017 Zac G. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
